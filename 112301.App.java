ce47b658fc778fd5361235f8d622fdc3229be819
25024426987 <74769176+25024426987@users.noreply.github.com>
2020-11-23 15:02:46 +0800

    Add files via upload

---
+++ 112301.App.java
@@ -0,0 +1,20 @@
+package pac;
+
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+		while (true) {
+			int x = (int)(Math.random()*100);
+			int y = (int)(Math.random()*100);
+			//System.out.println("输入一个字母:");
+			Scanner scanner = new Scanner(System.in);
+			String a = scanner.next();
+			if(a.equals("a")){
+				System.out.println(x+","+y);
+			}else {
+				System.out.println("没有这个坐标！");
+			}
+		}
+	}
+}
